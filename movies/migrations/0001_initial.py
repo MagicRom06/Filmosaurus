3aaa839bd5dc2b3a5cfb536c77e65d783bafe6a4
MagicRom06 <romainhenry60@gmail.com>
2021-09-16 19:07:45 +0200

    fix flake8

--- movies/migrations/0001_initial.py
+++ movies/migrations/0001_initial.py
@@ -15,37 +15,60 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(
+                    default=uuid.uuid4,
+                    editable=False,
+                    primary_key=True,
+                    serialize=False)),
                 ('name', models.CharField(max_length=200)),
             ],
         ),
         migrations.CreateModel(
             name='Country',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(
+                    default=uuid.uuid4,
+                    editable=False,
+                    primary_key=True,
+                    serialize=False)),
                 ('name', models.CharField(max_length=255)),
             ],
         ),
         migrations.CreateModel(
             name='Person',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(
+                    default=uuid.uuid4,
+                    editable=False,
+                    primary_key=True,
+                    serialize=False)),
                 ('name', models.CharField(max_length=255)),
             ],
         ),
         migrations.CreateModel(
             name='Movie',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('imdb_id', models.PositiveIntegerField(default=None)),
+                ('id', models.UUIDField(
+                    default=uuid.uuid4,
+                    editable=False,
+                    primary_key=True,
+                    serialize=False)),
+                ('imdb_id', models.PositiveIntegerField(
+                    default=None)),
                 ('title', models.CharField(max_length=255)),
                 ('year', models.PositiveIntegerField()),
                 ('picture', models.TextField(null=True)),
                 ('plot', models.TextField()),
-                ('cast', models.ManyToManyField(related_name='cast', to='movies.Person')),
-                ('category', models.ManyToManyField(to='movies.Category')),
-                ('country', models.ManyToManyField(to='movies.Country')),
-                ('director', models.ManyToManyField(related_name='director', to='movies.Person')),
+                ('cast', models.ManyToManyField(
+                    related_name='cast',
+                    to='movies.Person')),
+                ('category', models.ManyToManyField(
+                    to='movies.Category')),
+                ('country', models.ManyToManyField(
+                    to='movies.Country')),
+                ('director', models.ManyToManyField(
+                    related_name='director',
+                    to='movies.Person')),
             ],
         ),
     ]
--- movies/migrations/0002_auto_20210916_0826.py
+++ movies/migrations/0002_auto_20210916_0826.py
@@ -13,21 +13,37 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='category',
             name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.BigAutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name='ID'),
         ),
         migrations.AlterField(
             model_name='country',
             name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.BigAutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name='ID'),
         ),
         migrations.AlterField(
             model_name='movie',
             name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.BigAutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name='ID'),
         ),
         migrations.AlterField(
             model_name='person',
             name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.BigAutoField(
+                auto_created=True,
+                primary_key=True,
+                serialize=False,
+                verbose_name='ID'),
         ),
     ]
