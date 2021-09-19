2a510df40b71f8efbfd97d0404cac3ea45c2ec1d
MagicRom06 <romainhenry60@gmail.com>
2021-09-19 09:45:24 +0200

    configure statics

--- core/settings.py
+++ core/settings.py
@@ -16,9 +16,11 @@ import environ
 import os
 import socket
 
+# config django-debug-toolbar
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 
+# config env
 env = environ.Env()
 environ.Env.read_env()
 
