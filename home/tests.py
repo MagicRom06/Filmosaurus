28d10174a0ed0221c3d8ce1791ddd5f0c8724f23
MagicRom06 <romainhenry60@gmail.com>
2021-09-19 09:26:50 +0200

    add tests for home page

--- home/tests.py
+++ home/tests.py
@@ -1,1 +1,31 @@
+from home.views import HomePageView
+from django.test import TestCase
+from django.urls import reverse, resolve
+
 # Create your tests here.
+
+
+class HomePageTests(TestCase):
+    """
+    Testing home page
+    """
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home/home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Bienvenue')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
