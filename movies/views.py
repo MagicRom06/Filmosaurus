993d080c8711f082b2e0a57428a2b8510ba3bf07
MagicRom06 <romainhenry60@gmail.com>
2021-09-27 08:38:59 +0200

    fix flake8

--- movies/views.py
+++ movies/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 
 # Create your views here.
 
+
 class SearchResultsListView(ListView):
     paginate_by = 12
     model = Movie
@@ -15,6 +16,7 @@ class SearchResultsListView(ListView):
         return Movie.objects.filter(
             Q(title__icontains=query)
         )
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['query'] = self.request.GET.get('q')
