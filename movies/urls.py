deb82304672ecb107a6a52aeaf0fbdc94459bcd5
MagicRom06 <romainhenry60@gmail.com>
2021-10-05 22:40:25 +0200

    add search for director and actor

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import (MovieDetailView, MovieRatingJsonView,
-                    SearchResultsListView, watchlistAddMovie,
-                    watchlistUpdateMovie)
+from .views import (
+    MovieDetailView,
+    MovieRatingJsonView,
+    SearchResultsListView,
+    watchlistAddMovie,
+    watchlistUpdateMovie,
+    MovieAdvancedSearchTemplateView,
+    MovieAdvancedSearchResultsByDirector)
 
 urlpatterns = [
     path(
@@ -13,6 +18,16 @@ urlpatterns = [
         'get/<int:pk>',
         MovieDetailView.as_view(),
         name='movie_detail'
+    ),
+    path(
+        'advanced/search/',
+        MovieAdvancedSearchTemplateView.as_view(),
+        name='advanced_search'
+    ),
+    path(
+        'advanced/search/results',
+        MovieAdvancedSearchResultsByDirector.as_view(),
+        name="advanced_results"
     ),
     path('rating/<int:pk>', MovieRatingJsonView.as_view(), name='rating'),
     path('save/', watchlistAddMovie, name='save'),
