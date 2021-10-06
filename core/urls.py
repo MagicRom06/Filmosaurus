606ec2e568147dd19f944ddf55ce1adadf05fbbb
MagicRom06 <romainhenry60@gmail.com>
2021-10-06 10:25:20 +0200

    add security parameter for deployment

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from . import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secret_admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),
     path('movies/', include('movies.urls')),
@@ -31,4 +32,7 @@ if settings.DEBUG:
 
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    ] + urlpatterns + static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
