3583dcefdde07cf979431de936fb49cf4fc28b28
MagicRom06 <romainhenry60@gmail.com>
2021-10-06 20:36:13 +0200

    change debug and environment settings + add hosts

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.180.133.182', 'localhost']
 
 
 # Application definition
