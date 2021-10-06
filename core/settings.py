62abfd4df8a6878501ea1e6cfafc1841f8ba0139
MagicRom06 <romainhenry60@gmail.com>
2021-10-06 22:27:45 +0200

    db dump + static_root settings

--- core/settings.py
+++ core/settings.py
@@ -166,7 +166,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_FINDER = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
@@ -224,3 +223,4 @@ if ENVIRONMENT == 'production':
     # SESSION_COOKIE_SECURE = True
     # CSRF_COOKIE_SECURE = True
     # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
